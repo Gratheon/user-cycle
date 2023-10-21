53c3f3cc1a2f8417cb78245c53fa6b98d439f4e8
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-10-22 01:11:32 +0300

    simplify makefile, cleanup migrations

--- migrations/007-translations.sql
+++ migrations/007-translations.sql
@@ -1,28 +1,4 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 5446
-#
-# https://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 8.1.0)
-# Database: swarm-user
-# Generation Time: 2023-10-08 20:26:55 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 SET NAMES utf8mb4;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table locales
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `locales`;
 
@@ -43,7 +19,6 @@ CREATE TABLE `locales` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `locales` WRITE;
-/*!40000 ALTER TABLE `locales` DISABLE KEYS */;
 
 INSERT INTO `locales` (`id`, `key`, `en`, `ru`, `et`, `translation_context`, `date_added`, `tr`, `pl`)
 VALUES
@@ -107,14 +82,4 @@ VALUES
 	(77,NULL,'Password','Пароль','Parool','','2023-10-08 00:23:03','Parola','Hasło'),
 	(78,NULL,'Register','Зарегистрироваться','Registreeri','','2023-10-08 00:24:17','Kaydol','Zarejestruj się');
 
-/*!40000 ALTER TABLE `locales` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- migrations/008-fr-de-locale.sql
+++ migrations/008-fr-de-locale.sql
@@ -1,28 +1,4 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 5446
-#
-# https://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 8.1.0)
-# Database: swarm-user
-# Generation Time: 2023-10-09 21:58:56 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 SET NAMES utf8mb4;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table locales
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `locales`;
 
@@ -45,7 +21,6 @@ CREATE TABLE `locales` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `locales` WRITE;
-/*!40000 ALTER TABLE `locales` DISABLE KEYS */;
 
 INSERT INTO `locales` (`id`, `key`, `en`, `ru`, `et`, `translation_context`, `date_added`, `tr`, `pl`, `fr`, `de`)
 VALUES
@@ -108,14 +83,4 @@ VALUES
 	(77,NULL,'Password','Пароль','Parool','','2023-10-08 00:23:03','Parola','Hasło','Mot de passe','Passwort'),
 	(78,NULL,'Register','Зарегистрироваться','Registreeri','','2023-10-08 00:24:17','Kayıt Ol','Zarejestruj się','S\'inscrire','Registrieren');
 
-/*!40000 ALTER TABLE `locales` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
