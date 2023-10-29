333583f0faadff88c2c33e015b257dafb318ddda
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-10-30 01:21:17 +0200

    add translations

---
+++ migrations/009-llm-feature-translations.sql
@@ -0,0 +1,6 @@
+INSERT INTO `locales` (`id`, `key`, `en`, `ru`, `et`, `tr`, `pl`, `fr`, `de`, `translation_context`, `date_added`)
+VALUES
+	(82, NULL, 'Need advice from AI?', 'Нужен совет от искусственного интеллекта?', 'Vajad nõuandeid AI-lt?', 'Yapay zekadan tavsiye almak mı gerekiyor?', 'Potrzebujesz porady od AI?', 'Besoin de conseils de l\'IA ?', 'Brauchen Sie Ratschläge von KI?', '', '2023-10-29 23:17:45'),
+	(81, NULL, 'Review', 'Обзор', 'Ülevaade', 'İnceleme', 'Przegląd', 'Examen', 'Überprüfung', '', '2023-10-29 23:17:45'),
+	(80, NULL, 'Worker bees', 'Рабочие пчелы', 'Töömesilased', 'İşçi arılar', 'Robotnice', 'Les abeilles ouvrières', 'Arbeiterbienen', 'this is a button that toggles visibility of worker bees on an image', '2023-10-25 21:52:54'),
+	(79, NULL, 'Drones', 'Трутни', 'Isendid', 'Arılar', 'Drony', 'Drones', 'Drohnen', 'this is a button that toggles visibility of drone bees on an image', '2023-10-25 21:52:54');
