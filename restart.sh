53b17b3ef5a6378c4e96e4fb0d4069b2064c8290
Artjom Kurapov <artkurapov@gmail.com>
2024-09-07 01:18:38 +0300

    fix build

--- restart.sh
+++ restart.sh
@@ -1,3 +1,4 @@
 cd /www/user-cycle/
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down
+rm -rf ./app
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml up --build -d
