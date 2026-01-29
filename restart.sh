0bd82359d6e83585f3851c3e019bab4306862419
Artjom Kurapov <artkurapov@gmail.com>
2026-01-29 12:06:27 +0200

    Revert --remove-orphans flag that stopped other services
    The --remove-orphans flag caused docker-compose to stop all other
    services in the gratheon project when deploying user-cycle.
    This is because they share COMPOSE_PROJECT_NAME=gratheon but are
    in separate docker-compose files.

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 cd /www/user-cycle/
-COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down --remove-orphans
+COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down
 docker rm -f gratheon_user-cycle_1 2>/dev/null || true
 rm -rf ./app
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml up --build -d
