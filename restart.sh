9f1f15e253c0bf9e138ead756439b2d4c8a753b8
Artjom Kurapov <artkurapov@gmail.com>
2026-01-29 11:39:02 +0200

    Fix restart script to remove stale containers
    - Add --remove-orphans flag to docker-compose down
    - Force remove container by name before recreating to avoid conflicts
    - This fixes deployment failures when containers aren't fully cleaned up

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 cd /www/user-cycle/
-COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down
+COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml down --remove-orphans
+docker rm -f gratheon_user-cycle_1 2>/dev/null || true
 rm -rf ./app
 COMPOSE_PROJECT_NAME=gratheon docker-compose -f docker-compose.prod.yml up --build -d
